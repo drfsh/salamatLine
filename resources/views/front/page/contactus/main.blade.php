fbeee22cb2902712c88dea84f0c45dd5d3c03260
drfsh <mahdidera63@gmail.com>
2022-09-30 12:31:12 +0330

    Update main.blade.php

--- resources/views/front/page/contactus/main.blade.php
+++ resources/views/front/page/contactus/main.blade.php
@@ -91,7 +91,7 @@
                         </div>
                     </div>
                     <div class="c-map">
-                        <map-page lng="{{$globalcontact[0]->long}}" lat="{{$globalcontact[0]->lat}}" ></map-page>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d810.0981830184171!2d51.403563706773845!3d35.69195202476301!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8e0109e6bb2745%3A0x7e7500dd41ba395f!2sSalamatLine!5e0!3m2!1sfa!2s!4v1664528232430!5m2!1sfa!2s" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
             </div>
