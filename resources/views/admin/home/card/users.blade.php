739f043ea32be6407f0ec752755c0b8f87965b34
drfsh <mahdidera63@gmail.com>
2022-10-03 12:59:39 +0330

    Update users.blade.php

--- resources/views/admin/home/card/users.blade.php
+++ resources/views/admin/home/card/users.blade.php
@@ -1,7 +1,7 @@
 @if(sizeof($data['userTypes'])>0)
     <div class="card-3-part">
     <div class="c1">کاربران آنلاین</div>
-    <div class="c2">{{$data['userTypes'][1]['sessions']}} نفر لاگین شدن </div>
-    <div class="c3">{{$data['userTypes'][0]['sessions']}} لاگین نشدن </div>
+    <div class="c2">0 نفر لاگین شدن </div>
+    <div class="c3">0 لاگین نشدن </div>
 </div>
 @endif
